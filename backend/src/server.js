165de4eab71c16cccad035fdedbab69609b98884
Animesh Samantaray <animeshsamantaray2@gmail.com>
2025-09-25 21:10:04 +0530

    added rate-limitation

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import NotesRoutes from './routes/NotesRoutes.js'
 import { connectDb } from './config/db.js';
 import dotenv from 'dotenv';
-
+import rateLimiter from './middleware/rateLimiter.js';
 dotenv.config();
 const port = process.env.PORT || 5001;
 
@@ -10,7 +10,7 @@ const app=express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(rateLimiter)
 connectDb();
 app.use('/api/notes' ,NotesRoutes );
 app.listen(port,(req,res)=>{
